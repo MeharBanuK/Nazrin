781745d498919e53530b1abd6400c920d3658189
MeharBanuK <banumehar5@gmail.com>
2017-01-29 12:22:43 +0530

    Add files via upload

---
+++ EnumE2.java
@@ -0,0 +1,12 @@
+enum Season { WINTER, SPRING, SUMMER, FALL }  
+class EnumE2
+{  
+public static void main(String[] args) 
+{  
+Season s=Season.WINTER;
+Season s1=Season.SPRING;
+Season s2=Season.SUMMER;
+Season s3=Season.FALL;  
+System.out.println(s+"\t"+s1+"\t"+s2+"\t"+s3);  
+}
+} 
